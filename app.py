6bb20d95e39fc15f6005ca6be1424566798de72a
abdelkader <abdelkaderboukart@gmail.com>
2023-11-06 12:56:11 +0100

    m app.py add_ user.html

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from waitress import serve
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, equal_to, Length
 from flask_bcrypt import Bcrypt  # Import Bcrypt
 from flask_login import UserMixin, login_user, login_manager, logout_user, login_required, current_user, login_remembered
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -17,7 +17,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     neme = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(60), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
     
     @property
     def password(self):
@@ -37,7 +37,8 @@ class User(db.Model):
 class userForm(FlaskForm):
     name = StringField("name", validators=[DataRequired()])
     email = StringField("email", validators=[DataRequired()])
-    password = PasswordField("password", validators=[DataRequired()]) 
+    password_hash = PasswordField("password", validators=[DataRequired(), equal_to('password_hash2', message='password must match!')])
+    password_hash2 = PasswordField("password", validators=[DataRequired()]) 
     submit = SubmitField('login') 
 
 class loginForm(FlaskForm):
@@ -75,7 +76,7 @@ def add_user():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = User(name=form.name.data, email=form.email.data, password_hash=form.password.data)
+            user = User(name=form.name.data, email=form.email.data, password_hash=form.password_hash.data)
             db.session.add(user)
             db.session.commit()
 
@@ -88,7 +89,8 @@ def add_user():
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
-        form.password.data = ''
+        form.password_hash.data = ''
+        form.password_hash2.data = ''
         return render_template('add_user.html',
                                form = form,
                                name = name)
